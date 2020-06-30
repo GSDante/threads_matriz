98ca3836b895c9f65146237605d99b2da661d34a
Noé Fernandes <noepessoa@outlook.com>
2020-06-30 16:10:12 -0300

    criando primeira versão da solucao sequencial e concorrente

---
+++ mult_mat2.py
@@ -0,0 +1,37 @@
+import threading
+
+def func(lin_a, matriz_b, lin_res):
+	for j in range(len(lin_a)):
+		soma = 0
+		for k in range(len(lin_a)):
+			soma += lin_a[k]*matriz_b[k][j]
+		lin_res[j] = soma
+
+def multiplicar(matriz_a, matriz_b, resultado, metodo):
+	dim = len(matriz_a)
+	if(metodo == 'S'):
+		soma = 0
+		for i in range(dim):
+			for j in range(dim):
+				soma = 0
+				for k in range(dim):
+					soma += matriz_a[i][k]*matriz_b[k][j]
+				resultado[i][j] = soma
+	elif(metodo == 'C'):
+		print('Em desenvolvimento...')
+		threads = []
+		for i in range(dim):
+			t = threading.Thread(target=func, args=(matriz_a[i], matriz_b, resultado[i]))
+			threads.append(t)
+			t.start()
+		for thread in threads:
+			thread.join()
+	else:
+		print('Erro ao escolher o método para efetuar o cálculo.')
+
+if(__name__ == '__main__'):
+	ma = [[23, 65, 9], [12, 2, 7], [76, 84, 32]]	  
+	mb = [[35, 24, 8], [10, 36, 15], [17, 76, 43]]
+	res = [[None, None, None], [None, None, None], [None, None, None]]
+	multiplicar(ma, mb, res, 'C')
+	print(res)
