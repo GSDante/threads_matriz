b1ff8524da2f967120abb2aa0728388454855770
Giovanne <giovannedssantos@gmail.com>
2020-06-30 09:41:37 -0300

    Escopo do programa

---
+++ mult_mat.py
@@ -0,0 +1,14 @@
+import numpy as np 
+from threading import Thread
+
+#Multiplicação de matrizes de modo sequencial
+def produto_matriz_seq()
+
+
+
+#Multiplicação de matrizes de modo concorrente
+def produto_matriz_con
+
+
+if __name__ == '__main__':
+	main()
